7e015c56fe6fe78c53bd8f670cf8df62080ea524
yeongjun Kim <kyjun519@gmail.com>
2023-05-26 06:01:25 +0900

    feat: Home 페이지 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
+import Layout from "@/components/layout";
+import Modal from "@/components/modal";
+import { modalState } from "@/lib/recoilState";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+
 export default function Home() {
-  return <>Home</>;
+  const [reservation, setReservation] = useRecoilState(modalState);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setReservation(1);
+    }, 8000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <Layout>
+      <div className="pt-16 px-2 h-[36rem] flex justify-center relative">
+        {reservation ? <Modal /> : ""}
+        <div className="absolute top-36">예약 대기 중...</div>
+      </div>
+      {reservation ? (
+        <div className="bg-[rgba(0,0,0,0.2)] fixed top-0 left-0 z-10 w-screen h-screen"></div>
+      ) : (
+        ""
+      )}
+    </Layout>
+  );
 }
